dcf5f331ab225129f980b2325e4a45a0183a6de7
Craig Harman <324026+craigharman@users.noreply.github.com>
2022-07-19 20:46:38 +0800

    feat: :sparkles: logging is done via DIrectus logger instance
    Swap out console logs for Directus's Pino instance.

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { createCollection, updateTypes } from "./nuxtRemote"
 
 import { defineHook } from "@directus/extensions-sdk"
 
-export default defineHook(({ action }) => {
-	action("collections.create", createCollection)
+export default defineHook(({ action }, { logger }) => {
+	action("collections.create", (collection) =>
+		createCollection(collection, logger)
+	)
 
-	action("fields.create", updateTypes)
-	action("fields.update", updateTypes)
-	action("fields.delete", updateTypes)
+	action("fields.create", () => updateTypes(logger))
+	action("fields.update", () => updateTypes(logger))
+	action("fields.delete", () => updateTypes(logger))
 })
--- src/nuxtRemote.ts
+++ src/nuxtRemote.ts
@@ -1,23 +1,27 @@
+import { Logger } from "pino"
 import axios from "axios"
 
 const nuxtServer: string = process.env.NUXT_SERVER || "http://localhost:3000"
 
-export const createCollection = async function (collection: any) {
+export const createCollection = async function (
+	collection: any,
+	logger: Logger
+) {
 	// Get types before creating page otherwise Nuxt restarts while getting them
-	await updateTypes()
-	console.log("Creating Nuxt page for " + collection.payload.collection)
+	await updateTypes(logger)
+	logger.info("Creating Nuxt page for " + collection.payload.collection)
 	return await axios.post(
 		`${nuxtServer}/api/directus/collection`,
 		collection.payload
 	)
 }
 
-export const updateTypes = async function () {
-	console.log("Updating types in Nuxt due to field change.")
+export const updateTypes = async function (logger: Logger) {
+	logger.info("Updating types in Nuxt due to field change.")
 	try {
 		return await axios.post(`${nuxtServer}/api/directus/field`)
 	} catch (err: any) {
-		console.error(err.message)
+		logger.error(err.message)
 		return
 	}
 }
